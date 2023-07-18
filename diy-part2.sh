d15e861b553134c7e0fb55491d9e9d0d18b43631
free-diy <71208560+free-diy@users.noreply.github.com>
2023-07-18 09:42:27 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -25,28 +25,28 @@ git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luc
 git clone https://github.com/sirpdboy/luci-theme-opentopd.git package/luci-theme-opentopd
 # 拉取中文版netdata
 git clone https://github.com/sirpdboy/luci-app-netdata package/luci-app-netdata
-#添加系统高级设置
+# 添加系统高级设置
 git clone https://github.com/sirpdboy/luci-app-advanced.git package/luci-app-advanced
-#添加ddns-go
+# 添加ddns-go
 git clone https://github.com/sirpdboy/luci-app-ddns-go.git package/ddns-go
 # 添加应用管理
 #git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
 # 添加vssr
 git clone https://github.com/jerrykuku/lua-maxminddb.git package/lean/lua-maxminddb
 git clone https://github.com/jerrykuku/luci-app-vssr.git package/lean/luci-app-vssr
-#添加adguardhome
+# 添加adguardhome
 svn co https://github.com/kenzok8/small-package/trunk/luci-app-adguardhome package/luci-app-adguardhome
-#添加smartdns
+# 添加smartdns
 svn co https://github.com/kenzok8/small-package/trunk/luci-app-smartdns package/luci-app-smartdns
-#添加bypass
+# 添加bypass
 svn co https://github.com/kenzok8/small-package/trunk/luci-app-bypass package/luci-app-bypass
-#添加passwall2
+# 添加passwall2
 svn co https://github.com/xiaorouji/openwrt-passwall2/trunk/luci-app-passwall2 package/luci-app-passwall2
-#添加poweroff
+# 添加poweroff
 svn co https://github.com/kenzok8/small-package/trunk/luci-app-poweroff package/luci-app-poweroff
-#添加OpenClash
+# 添加OpenClash
 svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/luci-app-openclash
-#添加istore
+# 添加istore
 svn co https://github.com/linkease/istore/trunk/luci/taskd package/taskd
 svn co https://github.com/linkease/istore/trunk/luci/luci-lib-xterm package/luci-lib-xterm
 svn co https://github.com/linkease/istore/trunk/luci/luci-lib-taskd package/luci-lib-taskd
