c0b25d66e67cf854805adf061b4525466828ef82
free-diy <71208560+free-diy@users.noreply.github.com>
2023-07-18 09:12:19 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -17,22 +17,25 @@ sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/lean/default-settings/files/zzz
 #
 # 删除老argon
 rm -rf feeds/luci/themes/luci-theme-argon
+# 删除英文版netdata
+rm -rf feeds/luci/applications/luci-app-netdata
 # 拉取argon主题
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
 # 拉取sirpdboy主题
 git clone https://github.com/sirpdboy/luci-theme-opentopd.git package/luci-theme-opentopd
-#
-#添加应用管理
-#git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
-#添加vssr
-git clone https://github.com/jerrykuku/lua-maxminddb.git package/lean/lua-maxminddb
-git clone https://github.com/jerrykuku/luci-app-vssr.git package/lean/luci-app-vssr
-#添加adguardhome
-svn co https://github.com/kenzok8/small-package/trunk/luci-app-adguardhome package/luci-app-adguardhome
+# 拉取中文版netdata
+git clone https://github.com/sirpdboy/luci-app-netdata package/luci-app-netdata
 #添加系统高级设置
 git clone https://github.com/sirpdboy/luci-app-advanced.git package/luci-app-advanced
 #添加ddns-go
 git clone https://github.com/sirpdboy/luci-app-ddns-go.git package/ddns-go
+# 添加应用管理
+#git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
+# 添加vssr
+git clone https://github.com/jerrykuku/lua-maxminddb.git package/lean/lua-maxminddb
+git clone https://github.com/jerrykuku/luci-app-vssr.git package/lean/luci-app-vssr
+#添加adguardhome
+svn co https://github.com/kenzok8/small-package/trunk/luci-app-adguardhome package/luci-app-adguardhome
 #添加smartdns
 svn co https://github.com/kenzok8/small-package/trunk/luci-app-smartdns package/luci-app-smartdns
 #添加bypass
