5ae5cbc3df6be14af3685927d56ee633a0d924c7
free-diy <71208560+free-diy@users.noreply.github.com>
2023-09-14 14:52:55 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,4 +1,4 @@
 # File name: diy-part1.sh
-echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall;packages" >> "feeds.conf.default"
-# echo "src-git passwall_luci https://github.com/xiaorouji/openwrt-passwall;luci" >> "feeds.conf.default"
+echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
+echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
 echo "src-git helloworld https://github.com/fw876/helloworld.git" >> "feeds.conf.default"
