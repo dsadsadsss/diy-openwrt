8a3143e38f302567f4bd51f1c285bd5872e3ed1d
free-diy <71208560+free-diy@users.noreply.github.com>
2024-01-11 13:41:46 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -65,7 +65,7 @@ luci/luci-lib-xterm \
 luci/luci-lib-taskd \
 luci/luci-app-store
 pushd luci
-mv -f */ ../
+mv -f */ ../../
 popd && rm -rf package/cache
 
 git clone --depth 1 --filter=blob:none -b main https://github.com/linkease/istore-ui.git package/cache
