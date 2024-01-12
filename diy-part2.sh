5bddb138b6828a5054c972e054f30fa61e30901c
free-diy <71208560+free-diy@users.noreply.github.com>
2024-01-12 18:48:12 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,24 +12,24 @@ rm -rf feeds/luci/themes/luci-theme-argon
 # 删除英文版netdata
 rm -rf feeds/luci/applications/luci-app-netdata
 # 拉取定时设置
-git clone https://github.com/sirpdboy/luci-app-autotimeset package/luci-app-autotimeset
+git clone --depth=1 https://github.com/sirpdboy/luci-app-autotimeset package/luci-app-autotimeset
 # 拉取argon主题
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
+git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
 # 拉取sirpdboy主题
-git clone https://github.com/sirpdboy/luci-theme-opentopd.git package/luci-theme-opentopd
+git clone --depth=1 https://github.com/sirpdboy/luci-theme-opentopd package/luci-theme-opentopd
 # 拉取中文版netdata
-git clone https://github.com/sirpdboy/luci-app-netdata.git package/luci-app-netdata
+git clone --depth=1 -b master https://github.com/sirpdboy/luci-app-netdata package/luci-app-netdata
 # 添加系统高级设置
-git clone https://github.com/sirpdboy/luci-app-advanced.git package/luci-app-advanced
+git clone --depth=1 https://github.com/sirpdboy/luci-app-advanced package/luci-app-advanced
 # 添加ddns-go
-git clone https://github.com/sirpdboy/luci-app-ddns-go.git package/ddns-go
+git clone --depth=1 https://github.com/sirpdboy/luci-app-ddns-go package/ddns-go
 # 添加应用管理
-#git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
+#git clone --depth=1 -b master https://github.com/destan19/OpenAppFilter package/OpenAppFilter
 # 添加vssr
-git clone https://github.com/jerrykuku/lua-maxminddb.git package/lean/lua-maxminddb
-git clone https://github.com/free-diy/luci-app-vssr.git package/lean/luci-app-vssr
+git clone --depth=1 https://github.com/jerrykuku/lua-maxminddb package/lean/lua-maxminddb
+git clone --depth=1 https://github.com/free-diy/luci-app-vssr package/lean/luci-app-vssr
 # eqosplus定时限速
-git clone https://github.com/sirpdboy/luci-app-eqosplus.git package/luci-app-eqosplus
+git clone --depth=1 https://github.com/sirpdboy/luci-app-eqosplus package/luci-app-eqosplus
 
 #####
 function git_sparse_clone() {
